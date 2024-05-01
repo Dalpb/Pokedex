2d36d81a0ef7d2758d1329282c1f31200880d402
Diego Palomino <dpalomino032002@gmail.com>
2024-05-01 16:53:23 -0500

    fixed

--- src/Components/PokeDisplay.jsx
+++ src/Components/PokeDisplay.jsx
@@ -3,21 +3,28 @@ import {useState,useEffect} from 'react'
 
 
 export function PokeDisplay({data}){
-    console.log(data)
+    const render =()=>{
+        return (
+            <>
+                <h1 id="p-name">{data.name}</h1>
+                <img id="p-img" src={data.srcImg} alt={`pokemon image -${data.id}`}/>
+                <div className="stats-container">
+                    <p><strong>Hp:</strong><span id="hp">{data.hp}</span></p>
+                    <p><strong>Attack:</strong><span id="attack">{data.attack}</span></p>
+                    <p><strong>Defense:</strong><span id="defense">{data.defense}</span></p>
+                    <p><strong>Special-Attack:</strong><span id="s-attack">{data.s_attack}</span></p>
+                    <p><strong>Special-Defense:</strong><span id="s-deffense">{data.s_defense}</span></p>
+                    <p><strong>Speed:</strong><span id="speed">{data.speed}</span></p>
+                </div>
+            </>
+        )
+    }
+
     return(
         <div className="display-container">
             <div className="border-displayer">
                 <div className="main-displayer">
-                    <h1 id="p-name">{data.name}</h1>
-                    <img id="p-img" src={data.srcImg} alt={`pokemon image -${data.id}`}/>
-                    <div className="stats-container">
-                        <p><strong>Hp:</strong><span id="hp">{data.hp}</span></p>
-                        <p><strong>Attack:</strong><span id="attack">{data.attack}</span></p>
-                        <p><strong>Defense:</strong><span id="defense">{data.defense}</span></p>
-                        <p><strong>Special-Attack:</strong><span id="s-attack">{data.s_attack}</span></p>
-                        <p><strong>Special-Defense:</strong><span id="s-deffense">{data.s_defense}</span></p>
-                        <p><strong>Speed:</strong><span id="speed">{data.speed}</span></p>
-                    </div>
+                {data.name === undefined ? <></>: render()}
                 </div>
             </div>
         </div>
